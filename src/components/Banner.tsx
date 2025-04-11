728ec14da3b94f7badd76dc536fd5b72105b9b93
Riyan <griyan115@gmail.com>
2025-04-11 23:55:37 +0530

    completed banner section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 export const Banner = () => {
   return (
-    <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(254,_235,_255,0.7),rgb(254,_202,_255,0.7),rgb(254,_181,_255,0.7),rgb(255,_219,_231,0.7),rgb(255,_244,_214,0.7),rgb(243,_252,_215,0.7),rgb(210,_245,_238,0.7),rgb(195,_239,_255,0.7),rgb(251,_239,_255,0.7))]">
+    <div className="py-3 text-center bg-[linear-gradient(to_right,#FEECFF,#FECAFF,#FEB5FF,#FFDBE7,#FFF4D6,#F3FCD7,#D2F5EE,#C3EFFF,#FBEFFF)]">
       <div className="container">
         <p className="font-medium">
           <span className="hidden sm:inline">
