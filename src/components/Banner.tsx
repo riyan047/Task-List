1f20b80af8b0bd2b371fdf576f774c2e658956ed
Riyan <griyan115@gmail.com>
2025-04-11 23:51:13 +0530

    completed banner section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,13 @@
 export const Banner = () => {
-  return null;
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(254,_235,_255,0.7),rgb(254,_202,_255,0.7),rgb(254,_181,_255,0.7),rgb(255,_219,_231,0.7),rgb(255,_244,_214,0.7),rgb(243,_252,_215,0.7),rgb(210,_245,_238,0.7),rgb(195,_239,_255,0.7),rgb(251,_239,_255,0.7))]">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned inteface -
+          </span>
+          <a href="#" className="underline underline-offset-4"> Explore the demo</a>
+        </p>
+      </div>
+    </div>)
 };
