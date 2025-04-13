733f872b8d0ffb299cc6a39928cbb27a741865a0
Riyan <griyan115@gmail.com>
2025-04-14 00:37:19 +0530

    added faq's section

--- src/components/FAQs.tsx
+++ src/components/FAQs.tsx
@@ -1,3 +1,9 @@
+'use client'
+import { useState } from 'react';
+import PlusIcon from '../assets/icons/plus.svg'
+import MinusIcon from '../assets/icons/minus.svg'
+import clsx from 'clsx';
+
 const items = [
   {
     question: "What payment methods do you accept?",
@@ -21,6 +27,41 @@ const items = [
   },
 ];
 
+const AccordianItem = ({ question, answer }: { question: string; answer: string }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div key={question} className='py-7 border-b border-white/30'
+      onClick={() => { setIsOpen(!isOpen) }}>
+      <div className='flex items-center '>
+        <span className='flex-1 text-lg font-bold'>{question}</span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+        
+      </div>
+      <div
+        className={clsx(
+          'mt-4 transition-all duration-300 ease-in-out overflow-hidden',
+          isOpen ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'
+        )}
+      >
+        {answer}
+      </div>
+
+    </div>
+  )
+}
+
 export const FAQs = () => {
-  return null;
+  return (
+    <div className="bg-black text-white bg-gradient-to-b from-[#4C248A_34%] to-black py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className='text-center text-5xl sm:text-6xl sm:max-w-[648px] mx-auto tracking-tighter font-bold'>Frequently asked questions</h2>
+        <div className='mt-12 max-w-[648px] mx-auto'>
+          {items.map(({ question, answer }) => (
+            <AccordianItem question={question} answer={answer} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 };
